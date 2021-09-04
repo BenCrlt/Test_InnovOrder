3f6fe677e7b721c7e795e82e90fb8269eb960050
BenCrlt <benoitcournault@gmail.com>
2021-09-04 16:14:12 +0200

    Delete route in user controller

--- api-innovorder/src/user/user.controller.ts
+++ api-innovorder/src/user/user.controller.ts
@@ -7,19 +7,4 @@ import { UserService } from './user.service';
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
-
-    @Get('id/:id')
-    async findUserByID(@Param('id') id: string) {
-        return await this.userService.findUserByID(id);
-    }
-
-    @Get('name/:username')
-    async findUserByName(@Param('username') username: string) {
-        return await this.userService.findUserByName(username);
-    }
-
-    @Post()
-    async create(@Body() createUserDto: CreateUserDto) {
-        return await this.userService.create(createUserDto);
-    }
 }
